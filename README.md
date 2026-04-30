7cd83b9c684a8c203f8985c59395844a14a43f7b
CharlesDesc06 <charles.deschamps06@hotmail.com>
2026-04-30 15:34:48 +0200

    fix phone connection

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ https://charlesdesc06.github.io/web-cloud-ynov/
 - Facebook
 - Connexion anonyme
 
+## Comptes de test
+
+| Methode | Identifiant | Code |
+|---|---|---|
+| Telephone (OTP) | +33 6 12 34 56 78 | 123456 |
+
 ## Lancer en local
 
 ```bash
--- auth_phone.js
+++ auth_phone.js
@@ -2,17 +2,32 @@ import "./firebaseConfig";
 import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
 
 const auth = getAuth();
+let recaptchaVerifier = null;
 
-export const sendPhoneCode = (phoneNumber) => {
-  const recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha-container", {
-    size: "invisible",
-  });
-  return signInWithPhoneNumber(auth, phoneNumber, recaptchaVerifier);
+const getRecaptchaVerifier = () => {
+  if (!recaptchaVerifier) {
+    recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha-container", {
+      size: "invisible",
+      "expired-callback": () => {
+        recaptchaVerifier = null;
+      },
+    });
+  }
+  return recaptchaVerifier;
+};
+
+export const sendPhoneCode = async (phoneNumber) => {
+  try {
+    const verifier = getRecaptchaVerifier();
+    return await signInWithPhoneNumber(auth, phoneNumber, verifier);
+  } catch (error) {
+    recaptchaVerifier = null;
+    throw error;
+  }
 };
 
 export const verifyPhoneCode = (confirmationResult, code) => {
   return confirmationResult.confirm(code).then((result) => {
-    console.log("signin with phone success");
     return result.user;
   });
 };
